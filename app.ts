dfb45b52f464038355c91409246dc5440ad654d8
root <root@ip-10-0-0-158.ap-southeast-1.compute.internal>
2018-10-04 06:36:56 +0000

    restart配置

--- app.ts
+++ app.ts
@@ -3,8 +3,8 @@
 module.exports = app => {
   if (app.config.env === 'local') {
     app.beforeStart(async () => {
-      const ctx = await app.createAnonymousContext();  //创建匿名参数
-      await app.startbot(ctx);
+//      const ctx = await app.createAnonymousContext();  //创建匿名参数
+  //    await app.startbot(ctx);
       await app.model.sync({ force: false });
     });
   }
